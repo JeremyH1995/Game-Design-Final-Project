71f78ad25dae57a2363982ccb1992ffb484a7e02
Jeremy Heil <40702215+JeremyH1995@users.noreply.github.com>
2022-03-29 17:46:12 -0500

    fixed animation controller

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -2,20 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zombie : MonoBehaviour
+public class Knight : MonoBehaviour
 {
     Animator animator;
+    //stats
+    float speed = 1.0f;
+    int health = 250;
+    int damage = 20;
     // Start is called before the first frame update
     void Start()
     {
         animator = this.GetComponent<Animator>();
-        animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("Characters/Zombie/Animations/Zombie");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(animator.GetBool("isWalking") == false && animator.GetBool("isDead") == false && animator.GetBool("isAttacking") == false){
+        if(animator.GetBool("isWalking") == true){
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        else if(animator.GetBool("isWalking") == false && animator.GetBool("isDead") == false && animator.GetBool("isAttacking") == false){
             animator.SetBool("isWalking", true);
         }
     }
--- Assets/Scripts/Zombie.cs
+++ Assets/Scripts/Zombie.cs
@@ -2,22 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Knight : MonoBehaviour
+public class Zombie : MonoBehaviour
 {
     Animator animator;
+    //stats
+    float speed = 1.0f;
+    int health = 100;
     // Start is called before the first frame update
     void Start()
     {
-        animator = GetComponent<Animator>();
-        animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load("../Characters/Knight/animations/KnightController");
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(animator.GetBool("isWalking") == true){
+            transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+        }
         if(animator.GetBool("isWalking") == false && animator.GetBool("isDead") == false && animator.GetBool("isAttacking") == false){
             animator.SetBool("isWalking", true);
-        }
+        }  
     }
 
     void onCollisionEnter(Collision coll){
